fbf648faaf22c5c9dfbdc6f10dc98c7accab13f4
Kumar <ITSDKUMA@ljmu.ac.uk>
2024-09-10 13:04:43 +0100

    Feat: updated app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,8 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-import { FruitsComponent } from './components/fruits/fruits.component';
-import { HeaderComponent } from './components/header/header.component';
-import { VegitablesComponent } from './components/vegitables/vegitables.component';
-import { NavbarService } from './services/navbar.service';
-import { CartComponent } from './components/cart/cart.component';
-
-
 
 @NgModule({
-  imports: [
-    CommonModule,
-    RouterOutlet,
-    HeaderComponent,
-    FruitsComponent, 
-    VegitablesComponent,
-    CartComponent
-  ],
+  imports: [],
   declarations: [],
-  providers: [
-    NavbarService  
-]
+  providers: []
 })
 export class AppModule { }
